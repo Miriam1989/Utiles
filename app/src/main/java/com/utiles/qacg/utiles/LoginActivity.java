7ddbc789130759c8ffcb0e0da16537ace67af064
Jhovany Olvera <ingenieroolvera2010@gmail.com>
2016-12-25 17:46:10 -0600

    Implementacion de etiquetas de orden
    Por favor de utlilizarlas correctamente.

--- app/src/main/java/com/utiles/qacg/utiles/LoginActivity.java
+++ app/src/main/java/com/utiles/qacg/utiles/LoginActivity.java
@@ -5,9 +5,45 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //<editor-fold des=" * * * * *  U I    R E F E R E N C E S  * * * * * ">
+
+    //</editor-fold>
+
+    //<editor-fold des=" * * * * *  I N T E R N A L   V A R I A B L E S  * * * * * ">
+
+    //</editor-fold>
+
+    //<editor-fold des=" * * * * *  S E R V I C E S  A N D  C A L L B A C K S  * * * * * ">
+
+    //</editor-fold>
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+
+    //<editor-fold des=" * * * * *  I N I T   E L E M E N T S  * * * * * ">
+
+    //</editor-fold>
+
+
+    //<editor-fold des=" * * * * *  O N   C L I C K  E L E M E N T S  * * * * * ">
+
+    //</editor-fold>
+
+    //<editor-fold des=" * * * * *  M E N U   E L E M E N T S  * * * * * ">
+
+    //</editor-fold>
+
+    //<editor-fold des=" * * * * *  E V E N T     V I E W  * * * * * ">
+
+    //</editor-fold>
+
+
+    //<editor-fold des=" * * * * *  L I F E C Y C L E  * * * * * ">
+
+    //</editor-fold>
 }
