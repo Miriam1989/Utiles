eea5a92a8616e009bf9a83fc41d45af6df49d13f
Miriam <ing.martinezperezmiriam@gmail.com>
2016-12-25 17:26:57 -0600

    Inicio de proyecto
    Se inicia la pantalla de "Login"

---
+++ app/src/main/java/com/utiles/qacg/utiles/view/ListUser.java
@@ -0,0 +1,15 @@
+package com.utiles.qacg.utiles.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.utiles.qacg.utiles.R;
+
+public class ListUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_user);
+    }
+}
